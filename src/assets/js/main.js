8b39db080ab968a5fb1dd01c6fd8617fd6476ab6
Alex <avramenko.av84@gmail.com>
2018-07-10 14:26:51 +0300

    Initial commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,10 @@
+ //= ../../../node_modules/jquery/dist/jquery.js
+ //= ../../../node_modules/bootstrap/js/dist/util.js
+ //= ../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js
+
+
+
+
+
+
+
